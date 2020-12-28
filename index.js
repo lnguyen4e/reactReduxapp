12a697f19afd32244c2a28dd73326580ec1f3742
Lam B Nguyen <l0nguy10@louisville.edu>
2020-12-28 18:05:32 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+app.get('/', (req,res) => {
+    res.send({ Hi: "There" });
+});
+const PORT = process.env.PORT || 5000
+app.listen(PORT);
