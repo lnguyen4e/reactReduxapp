bbfdfd203005f4c1d4ec7dc40b4c451d4cdd7952
Lam B Nguyen <l0nguy10@louisville.edu>
2020-12-28 19:46:08 -0500

    new

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+require('./models/User');
+require('./service/passport');
+
+mongoose.connect(keys.mongoURI);
+
 const app = express();
-app.get('/', (req,res) => {
-    res.send({ Hi: "There" });
-});
+app.use(
+    cookieSession({
+        maxAge:1000*60*60,
+        keys:[keys.cookieKey]
+    })
+)
+app.use(passport.initialize());
+app.use(passport.session());
+require('./routes/authRoutes')(app)
+
 const PORT = process.env.PORT || 5000
 app.listen(PORT);
